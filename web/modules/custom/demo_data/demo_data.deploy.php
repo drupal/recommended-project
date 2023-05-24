48e75b1cd0a052b35b0c0229ac17efcb6f3fc63d
badeadrian96 <badeadrian96@gmail.com>
2023-05-24 15:49:04 +0300

    refs 17731 dev start for demo data module, added langueage, enable simple search form and added block on edw theme

---
+++ web/modules/custom/demo_data/demo_data.deploy.php
@@ -0,0 +1,20 @@
+<?php
+
+use Drupal\menu_link_content\Entity\MenuLinkContent;
+
+function demo_data_deploy_10002() {
+  $nid = 1;
+  $parent = MenuLinkContent::create([
+    'title' => 'About',
+    'link' => [['uri' => 'internal:/node/' . $nid]],
+    'menu_name' => 'main',
+  ]);
+  $parent->save();
+  $child1 = MenuLinkContent::create([
+    'title' => 'About the Programme',
+    'link' => [['uri' => 'internal:/node/2']],
+    'menu_name' => 'main',
+    'parent' => 'menu_link_content:' . $parent->uuid(),
+  ]);
+  $child1->save();
+}
