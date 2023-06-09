044259a663b889302178518d7fcd023958aa094d
Stefan Butura <stefanbutura9@gmail.com>
2023-06-09 17:35:47 +0300

    #17731 - phpcs

--- web/modules/custom/demo_data/demo_data.deploy.php
+++ web/modules/custom/demo_data/demo_data.deploy.php
@@ -1,9 +1,14 @@
 <?php
 
-use Drupal\menu_link_content\Entity\MenuLinkContent;
+/**
+ * @file
+ * Deploy hooks for demo data.
+ */
 
+/**
+ * Create demo data.
+ */
 function demo_data_deploy_10001() {
-
   $homepage = \Drupal::entityTypeManager()->getStorage('node')->create([
     'type' => 'page',
     'title' => 'Homepage',
@@ -63,63 +68,62 @@ function demo_data_deploy_10001() {
   $aboutMenu = \Drupal::entityTypeManager()
     ->getStorage('menu_link_content')
     ->create([
-    'title' => 'Who we are',
-    'link' => [['uri' => 'internal:/node/' . $about->id()]],
-    'menu_name' => 'main',
-    'bundle' => 'menu_link_content',
-  ]);
+      'title' => 'Who we are',
+      'link' => [['uri' => 'internal:/node/' . $about->id()]],
+      'menu_name' => 'main',
+      'bundle' => 'menu_link_content',
+    ]);
   $aboutMenu->save();
 
   $knowledgeMenu = \Drupal::entityTypeManager()
     ->getStorage('menu_link_content')
     ->create([
-    'title' => 'What we do',
-    'link' => [['uri' => 'internal:/node/' . $knowledge->id()]],
-    'menu_name' => 'main',
-    'bundle' => 'menu_link_content',
-  ]);
+      'title' => 'What we do',
+      'link' => [['uri' => 'internal:/node/' . $knowledge->id()]],
+      'menu_name' => 'main',
+      'bundle' => 'menu_link_content',
+    ]);
   $knowledgeMenu->save();
-
 
   $newsMenu = \Drupal::entityTypeManager()
     ->getStorage('menu_link_content')
     ->create([
-    'title' => 'News',
-    'link' => [['uri' => 'internal:/node/' . $knowledge->id()]],
-    'menu_name' => 'main',
-    'bundle' => 'menu_link_content',
-  ]);
+      'title' => 'News',
+      'link' => [['uri' => 'internal:/node/' . $knowledge->id()]],
+      'menu_name' => 'main',
+      'bundle' => 'menu_link_content',
+    ]);
 
   $newsMenu->save();
 
   $childMenu = \Drupal::entityTypeManager()
     ->getStorage('menu_link_content')
     ->create([
-    'title' => 'Our approach',
-    'link' => [['uri' => 'internal:/node/' . $child->id()]],
-    'menu_name' => 'main',
-    'bundle' => 'menu_link_content',
-    'parent' => 'menu_link_content:' . $knowledge->uuid(),
-  ]);
+      'title' => 'Our approach',
+      'link' => [['uri' => 'internal:/node/' . $child->id()]],
+      'menu_name' => 'main',
+      'bundle' => 'menu_link_content',
+      'parent' => 'menu_link_content:' . $knowledge->uuid(),
+    ]);
   $childMenu->save();
 
   $contactMenu = \Drupal::entityTypeManager()
     ->getStorage('menu_link_content')
     ->create([
-    'title' => 'Contact Us',
-    'link' => [['uri' => 'internal:/node/' . $contact->id()]],
-    'menu_name' => 'footer',
-  ]);
+      'title' => 'Contact Us',
+      'link' => [['uri' => 'internal:/node/' . $contact->id()]],
+      'menu_name' => 'footer',
+    ]);
 
   $contactMenu->save();
 
   $policyMenu = \Drupal::entityTypeManager()
     ->getStorage('menu_link_content')
     ->create([
-    'title' => 'Privacy/Site Policy',
-    'link' => [['uri' => 'internal:/node/' . $policy->id()]],
-    'menu_name' => 'footer',
-  ]);
+      'title' => 'Privacy/Site Policy',
+      'link' => [['uri' => 'internal:/node/' . $policy->id()]],
+      'menu_name' => 'footer',
+    ]);
 
   $policyMenu->save();
 }
