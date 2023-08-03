501c483a07a482f091a8de90a3b872e24b8a8a73
Stefan Butura <stefanbutura9@gmail.com>
2023-08-03 15:01:10 +0300

    Uninstall modules that should not be enabled on prod

--- web/sites/example.settings.local.php
+++ web/sites/example.settings.local.php
@@ -130,30 +130,6 @@ $settings['rebuild_access'] = TRUE;
  */
 $settings['skip_permissions_hardening'] = TRUE;
 
-/**
- * Exclude modules from configuration synchronization.
- *
- * On config export sync, no config or dependent config of any excluded module
- * is exported. On config import sync, any config of any installed excluded
- * module is ignored. In the exported configuration, it will be as if the
- * excluded module had never been installed. When syncing configuration, if an
- * excluded module is already installed, it will not be uninstalled by the
- * configuration synchronization, and dependent configuration will remain
- * intact. This affects only configuration synchronization; single import and
- * export of configuration are not affected.
- *
- * Drupal does not validate or sanity check the list of excluded modules. For
- * instance, it is your own responsibility to never exclude required modules,
- * because it would mean that the exported configuration can not be imported
- * anymore.
- *
- * This is an advanced feature and using it means opting out of some of the
- * guarantees the configuration synchronization provides. It is not recommended
- * to use this feature with modules that affect Drupal in a major way such as
- * the language or field module.
- */
-$settings['config_exclude_modules'] = ['devel', 'stage_file_proxy'];
-
 #
 # Project specific settings
 #
