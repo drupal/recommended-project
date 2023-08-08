b6dc13c04ee619ffae8e2f36c46da639dc1f2af9
badeadrian96 <badeadrian96@gmail.com>
2023-08-08 16:56:41 +0300

    refs #20065 refactor build function

--- web/modules/custom/edw/modules/edw_blocks/src/Plugin/Block/EdwSearchBlock.php
+++ web/modules/custom/edw/modules/edw_blocks/src/Plugin/Block/EdwSearchBlock.php
@@ -23,7 +23,19 @@ class EdwSearchBlock extends ViewsBlock {
   protected $entityTypeManager;
 
   /**
-   * {@inheritdoc}
+   * Creates a new EdwSearchBlock instance.
+   *
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   *   The service container for dependencies.
+   * @param array<string, mixed> $configuration
+   *   Configuration values for this plugin.
+   * @param string $plugin_id
+   *   The plugin's ID.
+   * @param mixed $plugin_definition
+   *   Details about the plugin from annotations.
+   *
+   * @return static
+   *   A new EdwSearchBlock instance.
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
@@ -33,7 +45,18 @@ class EdwSearchBlock extends ViewsBlock {
   }
 
   /**
-   * {@inheritdoc}
+   * Builds the block's configuration form.
+   *
+   * @param array<string, mixed> $form
+   *   The initial form array.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   *
+   * @return array<string, mixed>
+   *   The modified form array.
+   *
+   * @throws \Exception
+   *    Throws an exception.
    */
   public function blockForm($form, FormStateInterface $form_state) {
     $form = parent::blockForm($form, $form_state);
@@ -55,7 +78,12 @@ class EdwSearchBlock extends ViewsBlock {
   }
 
   /**
-   * {@inheritdoc}
+   * Handles the block's form submission.
+   *
+   * @param array<string, mixed> $form
+   *   The form array.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
    */
   public function blockSubmit($form, FormStateInterface $form_state) {
     $this->configuration['content_types'] = $form_state->getValue('content_types');
@@ -71,7 +99,9 @@ class EdwSearchBlock extends ViewsBlock {
       $content_types = implode('+', $this->configuration['content_types']);
     }
 
-    $this->context['type'] = new Context(new ContextDefinition(), $content_types);
+    $context_definition = new ContextDefinition();
+    $context = new Context($context_definition, $content_types);
+    $this->setContext('type', $context);
     return parent::build();
   }
 
